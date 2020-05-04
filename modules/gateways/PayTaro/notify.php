2e7e8c4fff252464956714f2af7f086cf299b87d
root <root@root.com>
2020-05-04 21:31:45 +0800

    update

--- modules/gateways/PayTaro/notify.php
+++ modules/gateways/PayTaro/notify.php
@@ -11,7 +11,7 @@ if(!class_exists('PayTaro')) {
     include("./class.php");
 }
 
-logModuleCall('payTaro', 'notify', '', json_encode($_REQUEST));
+logModuleCall('payTaro', 'notify', '', http_build_query($_POST));
 // log_result(http_build_query($_REQUEST));
 $GATEWAY 					= getGatewayVariables('PayTaro');
 $url						= $GATEWAY['systemurl'];
@@ -21,19 +21,15 @@ if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is a
 $appId						= $GATEWAY['appId'];
 $appSecret					= $GATEWAY['appSecret'];
 $payTaro                    = new PayTaro($appId, $appSecret);
-$strToSign = $payTaro->prepareSign($_REQUEST);
-$verify_result = $payTaro->verify($strToSign, $_REQUEST['sign']);
-$query = Capsule::table('tblinvoices')->where('id', $_REQUEST['out_trade_no'])->where('userid', $userid)->first();
-if ($query->status === 'Paid') {
-    die('SUCCESS');
-}
+$strToSign = $payTaro->prepareSign($_POST);
+$verify_result = $payTaro->verify($strToSign, $_POST['sign']);
 if(!$verify_result) { 
-    logTransaction($GATEWAY["name"],$_GET,"Unsuccessful");
-    die('FAIL');
+	logTransaction($GATEWAY["name"], $_POST, "Unsuccessful");
+	die('FAIL');
 } else {
-    $invoiceId = $_REQUEST['out_trade_no'];
-    $transid = $_REQUEST['trade_no'];
-    $paymentAmount = $_REQUEST['total_amount'] / 100;
+    $invoiceId = $_POST['out_trade_no'];
+    $transid = $_POST['trade_no'];
+    $paymentAmount = $_POST['total_amount'] / 100;
     $feeAmount = 0;
 
     //货币转换开始
@@ -42,16 +38,19 @@ if(!$verify_result) {
 
     //获取账单 用户ID
     $userinfo 	= \Illuminate\Database\Capsule\Manager::table('tblinvoices')->where('id', $invoiceId)->first();
+    if ($userinfo->status === 'Paid') {
+    	die('SUCCESS');
+    }
 
     //得到用户 货币种类
     $currency = getCurrency($userinfo->userid);
 
     // 转换货币
-    $paymentAmount = convertCurrency( $paymentAmount, $currencytype->id, $currency['id'] );
+    $paymentAmount = convertCurrency($paymentAmount, $currencytype->id, $currency['id']);
     // 货币转换结束
     checkCbTransID($transid);
-    addInvoicePayment($invoiceId,$transid,$paymentAmount,$feeAmount,'PayTaro');
-    logTransaction($GATEWAY["name"],$_REQUEST,"Successful-A");
+    addInvoicePayment($invoiceId, $transid, $paymentAmount, $feeAmount, 'PayTaro');
+    logTransaction($GATEWAY["name"], $_POST, "Successful-A");
     die('SUCCESS');
 }
 die('FAIL');
